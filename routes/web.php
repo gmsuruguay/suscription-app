4ed80440f2e871764392faa9bab9d90c0e1fbd2a
martin_dev <gmsuruguay@gmail.com>
2022-06-08 17:21:46 -0300

    se redirecciono a pagina de suscripciones

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('suscriptions.index');
 });
 
 Route::resource('suscriptions',SuscriptionController::class);
